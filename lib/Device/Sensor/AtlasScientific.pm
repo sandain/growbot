a197a5792b70ececc292115a145f618ae28a8dbf
Jason M. Wood <sandain@hotmail.com>
2020-07-05 05:48:07 +0000

    cleanup pod

--- lib/Device/Sensor/AtlasScientific.pm
+++ lib/Device/Sensor/AtlasScientific.pm
@@ -2,57 +2,57 @@
 
 =head1 NAME
 
-  Device::Sensor::AtlasScientific
+Device::Sensor::AtlasScientific - Driver for Atlas Scientific environmental sensors.
 
 =head1 SYNOPSIS
 
 
 =head1 DESCRIPTION
 
-  Device::Sensor::AtlasScientific is a driver for the Atlas Scientific series of
-  environmental sensors.
+Device::Sensor::AtlasScientific is a driver for the Atlas Scientific series of
+environmental sensors.
 
 =head1 DEPENDENCIES
 
-  Device::Sensor::AtlasScientific requires Perl version 5.10 or later.
+Device::Sensor::AtlasScientific requires Perl version 5.10 or later.
 
 =head1 FEEDBACK
 
 =head2 Reporting Bugs
 
-  Report bugs to the GitHub issue tracker at:
+Report bugs to the GitHub issue tracker at:
   https://github.com/sandain/growbot/issues
 
 =head1 AUTHOR - Jason M. Wood
 
-  Email sandain@hotmail.com
+Email sandain@hotmail.com
 
 =head1 COPYRIGHT AND LICENSE
 
-  Copyright (c) 2020  Jason M. Wood <sandain@hotmail.com>
+Copyright (c) 2020  Jason M. Wood <sandain@hotmail.com>
 
-  All rights reserved.
+All rights reserved.
 
-  Redistribution and use in source and binary forms, with or without
-  modification, are permitted provided that the following conditions are met:
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
 
-  1. Redistributions of source code must retain the above copyright notice,
-     this list of conditions and the following disclaimer.
-  2. Redistributions in binary form must reproduce the above copyright
-     notice, this list of conditions and the following disclaimer in the
-     documentation and/or other materials provided with the distribution.
+1. Redistributions of source code must retain the above copyright notice,
+   this list of conditions and the following disclaimer.
+2. Redistributions in binary form must reproduce the above copyright
+   notice, this list of conditions and the following disclaimer in the
+   documentation and/or other materials provided with the distribution.
 
-  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-  ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
-  LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-  SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-  POSSIBILITY OF SUCH DAMAGE.
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+POSSIBILITY OF SUCH DAMAGE.
 
 =cut
 
